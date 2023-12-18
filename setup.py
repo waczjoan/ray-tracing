c73d22b33f4be7afe7f13456d31757e45dfb33ac
waczyjoan <asia.wacz@gmail.com>
2023-12-18 23:08:19 +0100

    use only toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="ray_tracing",
-    version="0.0.1",
-    description="All the most important functions used to ray tracing.",
-    author="waczyjoan",
-    packages=find_packages(),
-)
